23f038cb7929a57eb8a81ba6bd1c1d6df4f41509
Nicholas McCullum <nicholasmccullum@gmail.com>
2019-10-07 13:34:03 -0300

    initial commit
    this is a group of core libraries for quantitative finance in Python

---
+++ core-libraries.py
@@ -0,0 +1,9 @@
+from iexfinance.stocks import Stock 
+from datetime import datetime, timedelta 
+import numpy as np 
+from iexfinance.stocks import get_historical_data
+import pandas as import pd 
+import matplotlib.pyplot as plt 
+import matplotlib.dates as mdates
+
+import seaborn as sns
