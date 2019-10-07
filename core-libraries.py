b5c9ae8c3780a0f50f0ff9ee5a58447c5174ca14
Nicholas McCullum <nicholasmccullum@gmail.com>
2019-10-07 13:46:09 -0300

    fixed pandas library import error

--- core-libraries.py
+++ core-libraries.py
@@ -2,7 +2,7 @@ from iexfinance.stocks import Stock
 from datetime import datetime, timedelta 
 import numpy as np 
 from iexfinance.stocks import get_historical_data
-import pandas as import pd 
+import pandas as pd 
 import matplotlib.pyplot as plt 
 import matplotlib.dates as mdates
 
