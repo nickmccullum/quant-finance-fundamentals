997c07828a197d93f78378715ecf48aec3bc0161
Nicholas McCullum <nicholasmccullum@gmail.com>
2019-10-25 07:59:35 -0300

    Create email-template.py - password is placeholder

---
+++ email-template.py
@@ -0,0 +1,15 @@
+import smtplib
+
+initialTime = datetime.datetime.now().strftime("%I%M%p on %B %d, %Y")
+content = 'Subject: {}\n\n{}'.format('Data Feeds Builder #'+str(builderNumber)+'- Initialized', 'The data feeds builder Python script has started run at ' + initialTime)
+mail = smtplib.SMTP('smtp.gmail.com', 587)
+mail.ehlo()
+
+mail.starttls()
+
+mail.login('support@suredividend.com','password')
+mail.sendmail('support@suredividend.com','nick@suredividend.com',content)
+#mail.sendmail('support@suredividend.com','ben@suredividend.com',content)
+
+#Close the connection.
+mail.close()
