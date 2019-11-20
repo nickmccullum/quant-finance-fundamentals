71720f23351045be89705c0f4eeae59ba9d89c48
Nicholas McCullum <nicholasmccullum@gmail.com>
2019-11-20 09:07:02 -0400

    copied in code

--- profiler.py
+++ profiler.py
@@ -1,1 +1,31 @@
-#placeholder
+import cProfile, pstats, io
+
+
+
+def profile(fnc):
+    
+    """A decorator that uses cProfile to profile a function"""
+    
+    def inner(*args, **kwargs):
+        
+        pr = cProfile.Profile()
+        pr.enable()
+        retval = fnc(*args, **kwargs)
+        pr.disable()
+        s = io.StringIO()
+        sortby = 'cumulative'
+        ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+        ps.print_stats()
+        print(s.getvalue())
+        return retval
+
+    return inner
+
+@profile
+def datafeeds_builder():    
+    ###################################
+    # Insert code to be profiled here
+    ###################################
+    pass
+    
+%time datafeeds_builder()
